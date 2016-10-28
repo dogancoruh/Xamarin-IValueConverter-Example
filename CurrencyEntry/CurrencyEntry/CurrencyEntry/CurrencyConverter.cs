857347df3327c61c0cf2cc84e8f1ef509f0c81f3
Dogan Coruh <dogancoruh@gmail.com>
2016-10-28 17:53:55 +0300

    no message

---
+++ CurrencyEntry/CurrencyEntry/CurrencyEntry/CurrencyConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CurrencyEntry.Util
+{
+    public class CurrencyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string textValue = value.ToString();
+            decimal decimalValue = decimal.Parse(textValue);
+            return string.Format(new CultureInfo("tr-TR"), "{0:n0}", decimalValue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text = value.ToString();
+
+            if (string.IsNullOrEmpty(text))
+                return 0;
+
+            try
+            {
+                return decimal.Parse(value.ToString(), new CultureInfo("tr-TR"));
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ CurrencyEntry/CurrencyEntry/CurrencyEntry/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Xamarin.Forms;
+
+namespace CurrencyEntry
+{
+    public partial class MainPage : ContentPage
+    {
+        public static readonly BindableProperty AmountProperty = BindableProperty.Create(nameof(Amount), typeof(decimal), typeof(MainPage), 0m);
+
+        public decimal Amount
+        {
+            get { return (decimal)GetValue(AmountProperty); }
+            set { SetValue(AmountProperty, value); }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+            Amount = 1000000;
+        }
+    }
+}
